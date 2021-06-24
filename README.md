0273dfe7aff50e92dadc24b47ad34151a9e55791
Tommy109 <tokutomi@degino.com>
2021-06-24 10:04:42 +0900

    chore: タスクランナーを velociraptor に切り替えた。

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## 準備
 
-1. タスクランナーとして [denon](https://github.com/denosaurs/denon)
+1. タスクランナーとして [velociraptor](https://velociraptor.run)
    を使ってます。事前にインストールする必要があります。
 2. .env ファイルに DISCORD_KEY を設定しておく必要があります。
 
 ```env
 #.env
-DISCORD_KEY=TOKEN of discord
+DISCORD_KEY=<TOKEN of Discord>
 ```
 
 ## 実行
--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,4 @@
+import "https://deno.land/x/dotenv/load.ts";
 import { startBot, DiscordenoMessage } from "https://deno.land/x/discordeno@12.0.0-rc.3/mod.ts";
 import { Logger } from "./logger.ts";
 import { commit } from "./bot_dict.ts";
@@ -7,6 +8,7 @@ const OBSERVATIONS: readonly string[] =
 
 if (!Deno.env.get("DISCORD_KEY")) {
   Logger.error("Not found Discord key.");
+  throw Error("Not found Discord key.")
 }
 
 startBot({
