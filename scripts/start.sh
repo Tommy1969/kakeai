ec5c64dea3a3e8945140134a355ca2afe9bd89a9
Tommy109 <tokutomi@degino.com>
2021-06-24 10:42:48 +0900

    chore: デーモナイズ (nohup) した。

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,6 @@ export DENO_INSTALL="/home/ec2-user/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
 cd /opt/app/kakeai
-nohup deno -L info run --allow-net --allow-read --allow-write --allow-env --unstable mod.ts &
+nohup deno -L info run --allow-net --allow-read --allow-write --allow-env --unstable mod.ts < /dev/null 2>&1 /dev/null &
 
 exit 0
